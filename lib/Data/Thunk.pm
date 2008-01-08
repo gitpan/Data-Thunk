0091bf56f6af28d821b6f9ecc0cc42a46b011e55
יובל קוג'מן (Yuval Kogman) <nothingmuch@woobling.org>
2008-01-08 03:25:55 +0000

    Import of NUFFIN/Data-Thunk-0.01 from CPAN.
    gitpan-cpan-distribution: Data-Thunk
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         NUFFIN/Data-Thunk-0.01.tar.gz
    gitpan-cpan-author:       NUFFIN
    gitpan-cpan-maturity:     released

--- lib/Data/Thunk.pm
+++ lib/Data/Thunk.pm
@@ -1,106 +1,271 @@
-package Data::Thunk;
-$Data::Thunk::VERSION = '0.01';
+#!/usr/bin/perl
 
-use 5.006;
+package Data::Thunk;
+
 use strict;
 use warnings;
-use Exporter::Lite;
-use Class::InsideOut qw( private register id );
-our @EXPORT = qw( lazy thunk );
 
-private _thunk => my %_thunk;
+use Scalar::Util qw(blessed);
 
-use overload fallback => 1, map {
-    $_ => \&force
-} qw( bool "" 0+ ${} @{} %{} &{} *{} );
+use base qw(Exporter);
 
-sub force (&) {
-    &{$_thunk{ id $_[0] }}
-}
+our $VERSION = "0.01";
+
+our @EXPORT = our @EXPORT_OK = qw(lazy force);
 
 sub lazy (&) {
-    my $cv = shift;
-    my $obj = register( bless \(my $s), __PACKAGE__ );
-    $_thunk{ id $obj } = $cv;
-    return $obj;
+	my $thunk = shift;
+	bless { code => $thunk }, "Data::Thunk::Code";
 }
 
-sub thunk (&) {
-    my $cv = shift;
-    my ($value, $forced);
-    my $obj = register( bless \(my $s), __PACKAGE__ );
-    $_thunk{ id $obj } = sub {
-        $forced ? $value : scalar (++$forced, $value = &$cv)
-    };
-    return $obj;
+my ( $vivify_code, $vivify_scalar ); # these lexicals go into the other packages' scopes
+
+sub force ($) {
+	my $val = shift;
+
+	if ( blessed($val) ) { 
+		if ( blessed($val) eq 'Data::Thunk::Code' ) {
+			return $val->$vivify_code;
+		} elsif ( blessed($val) eq 'Data::Thunk::ScalarValue' ) {
+			return $val->$vivify_scalar;
+		}
+	}
+
+	return $val;
 }
 
-1;
+{
+	package Data::Thunk::NoOverload;
+	# we temporarily bless into this to avoid overloading
+}
+
+{
+	package Data::Thunk::Code;
+	use Data::Swap ();
+	use UNIVERSAL::ref;
+
+	use overload (
+		fallback => 1, map {
+			$_ => $vivify_code = sub {
+				bless $_[0], "Data::Thunk::NoOverload";
+
+				my $tmp = $_[0]->{code}->();
+
+				if ( CORE::ref($tmp) ) {
+					local $@;
+					eval { Data::Swap::swap $_[0], $tmp };
+
+					if ( my $e = $@ ) {
+						# try to figure out where the thunk was defined
+						my $lazy_ctx = eval {
+							require B;
+							my $cv = B::svref_2object($_[0]->{code});
+							my $file = $cv->FILE;
+							my $line = $cv->START->line;
+							"in thunk defined at $file line $line";
+						} || "at <<unknown>>";
+
+						my $file = quotemeta(__FILE__);
+						$e =~ s/ at $file line \d+.\n$/ $lazy_ctx, vivified/; # becomes "vivified at foo line blah"..
+
+						require Carp;
+						Carp::croak($e);
+					}
+
+					return $_[0];
+				} else {
+					Data::Swap::swap $_[0], do { my $o = $tmp; \$o };
+					bless $_[0], "Data::Thunk::ScalarValue";
+					return $_[0];
+				}
+			},
+		} qw( bool "" 0+ ${} @{} %{} &{} *{} )
+	);
+
+	my $vivify_and_call = sub {
+		my $method = shift;
+		$_[0]->$vivify_code();
+		goto &{$_[0]->can($method)}
+	};
+
+	sub ref {
+		CORE::ref($_[0]->$vivify_code);
+	}
+
+	foreach my $sym (keys %UNIVERSAL::) {
+		no strict 'refs';
+		*{$sym} = eval "sub {
+			if ( Scalar::Util::blessed(\$_[0]) ) {
+				unshift \@_, \$sym;
+				goto \$vivify_and_call;
+			} else {
+				shift->SUPER::$sym(\@_);
+			}
+		}";
+	}
+
+	sub AUTOLOAD {
+		my ( $self, @args ) = @_;
+		my ( $method ) = ( our $AUTOLOAD =~ /([^:]+)$/ );
+		unshift @_, $method;
+		goto $vivify_and_call;
+	}
+
+	sub DESTROY {
+		# don't create the value just to destroy it
+	}
+}
+
+{
+	package Data::Thunk::ScalarValue;
+	use UNIVERSAL::ref;
+
+	use overload (
+        fallback => 1, map {
+			$_ => $vivify_scalar = sub {
+				my $self = $_[0];
+
+				# must rebless to something unoverloaded in order to get at the value
+				bless $self, "Data::Thunk::NoOverload";
+				my $val = $$self;
+				bless $self, __PACKAGE__;
+
+				# try to replace the container with the value wherever we found it
+				local $@; eval { $_[0] = $val }; # might be readonly;
+
+				$val;
+			}
+        } qw( bool "" 0+ ${} @{} %{} &{} *{} )
+    );
+
+	sub ref {
+		my $self = shift;
+		return;
+	}
+
+	sub DESTROY { }
+}
+
+__PACKAGE__
 
 __END__
 
+=pod
+
 =head1 NAME
 
-Data::Thunk - Calculate values on demand
+Data::Thunk - A sneakier Scalar::Defer ;-)
 
 =head1 SYNOPSIS
 
-    use Data::Thunk; # exports 'lazy' and 'thunk'
+	use Data::Thunk qw(lazy);
 
-    my ($x, $y);
-    my $lv = lazy { ++$x };     # a lazy-value
-    my $tv = thunk { ++$y };    # a thunk-value
+	my %hash = (
+		foo => lazy { $expensive },
+	);
 
-    print "$lv $lv $lv"; # 1 2 3
-    print "$tv $tv $tv"; # 1 1 1
+	$hash{bar}{gorch} = $hash{foo};
+
+	$hash{bar}{gorch}->foo; # vivifies the object
+
+	warn overload::StrVal($hash{foo}); # replaced with the value
 
 =head1 DESCRIPTION
 
-This module exports two functions, C<lazy> and C<thunk>, for building
-values that are evaluated on demand.
+This is an implementation of thunks a la L<Scalar::Defer>, but uses
+L<Data::Swap> and assignment to C<$_[0]> in order to leave a minimal trace of the thunk.
 
-=head2 lazy {...}
+In the case that a reference is returned from C<lazy { }> L<Data::Swap> can
+replace the thunk ref with the result ref, so all the references that pointed
+to the thunk are now pointing to the result (at the same address).
 
-Takes a block or a code reference, and returns an overloaded value.
-Each time that value is demanded, the block is evaluated again to
-yield a fresh result.
+If a simple value is returned then the thunk is swapped with a simple scalar
+container, which will assign the value to C<$_[0]> on each overloaded use.
 
-=head2 thunk {...}
+In this particular example:
 
-Like C<lazy>, except the value is computed at most once.  Subsequent
-evaluation will simly use the cached result.
+	my $x = {
+		foo => lazy { "blah" },
+		bar => lazy { [ "boink" ] },
+	};
 
-=head1 NOTES
+	$x->{quxx} = $x->{foo};
+	$x->{gorch} = $x->{bar};
 
-Unlike the C<tie> based L<Data::Lazy>, this module operates on I<values>,
-not I<variables>.  Therefore, assigning into C<$t> simply replaces the value.
+	warn $x->{bar};
+	warn $x->{foo};
+	warn $x->{quxx};
 
-The C<overload> based implementation also makes this module about 2x faster
-than L<Data::Lazy>.
+	use Data::Dumper;
+	warn Dumper($x);
 
-=head1 AUTHORS
+The resulting structure is:
 
-Audrey Tang E<lt>cpan@audreyt.orgE<gt>
+	$VAR1 = {
+          'bar' => [
+                     'boink'
+                   ],
+          'foo' => 'blah',
+          'gorch' => $VAR1->{'bar'},
+          'quxx' => 'blah'
+        };
 
-=head1 COPYRIGHT (The "MIT" License)
+Whereas with L<Scalar::Defer> the trampoline objects remain:
 
-Copyright 2002-2006 by Audrey Tang <cpan@audreyt.org>.
+	$VAR1 = {
+			  'bar' => bless( do{\(my $o = 25206320)}, '0' ),
+			  'foo' => bless( do{\(my $o = 25387232)}, '0' ),
+			  'gorch' => $VAR1->{'bar'},
+			  'quxx' => $VAR1->{'foo'}
+			};
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is fur-
-nished to do so, subject to the following conditions:
+This is potentially problematic because L<Scalar::Util/reftype> and
+L<Scalar::Util/blessed> can't be fooled. With L<Data::Thunk> the problem still
+exists before values are vivified, but not after.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+Furthermore this module uses L<UNIVERSAL::ref> instead of blessing to C<0>.
+Blessing to C<0> pretends that everything is a non ref (C<ref($thunk)> returns
+the name of the package, which evaluates as false), so deferred values that
+become objects don't appear to be as such.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FIT-
-NESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE X
-CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+=head1 EXPORTS
+
+=over 4
+
+=item lazy { ... }
+
+Create a new thunk.
+
+=item force
+
+Vivify the value and return the result.
+
+=back
+
+=head1 SEE ALSO
+
+L<Scalar::Defer>, L<Data::Lazy>, L<Data::Swap>, L<UNIVERSAL::ref>.
+
+=head1 VERSION CONTROL
+
+This module is maintained using Darcs. You can get the latest version from
+L<http://nothingmuch.woobling.org/code>, and use C<darcs send> to commit
+changes.
+
+=head1 AUTHOR
+
+Yuval Kogman E<lt>nothingmuch@woobling.orgE<gt>
+
+=head1 COPYRIGHT
+
+	Copyright (c) 2008 Yuval Kogman. All rights reserved
+	This program is free software; you can redistribute
+	it and/or modify it under the same terms as Perl itself.
 
 =cut
+
+
+
+=cut
+
+
--- t/01-basic.t
+++ t/01-basic.t
@@ -1,11 +1,28 @@
-use Test::More tests => 5;
+use Test::More tests => 14;
 use ok 'Data::Thunk';
 
-my ($x, $y);
-my $l = lazy { ++$x };
-my $t = thunk { ++$y };
+my $y = 0;
+my $l = lazy { ++$y };
 
-is($l, $t, "1 == 1");
-is($l, 2, "lazy is now 2");
-is($t, 1, "but thunk stays at 1");
-isnt($l, $t, "3 != 1");
+is($y, 0, "not triggered yet");
+
+is($l, 1, "lazy value");
+
+is($y, 1, "trigerred");
+
+my $forced = force lazy { 4 };
+is($forced, 4, 'force($x) works');
+is($forced, 4, 'force($x) is stable');
+is(force $forced, 4, 'force(force($x)) is stable');
+
+$SomeClass::VERSION = 42;
+sub SomeClass::meth { 'meth' };
+sub SomeClass::new { bless(\@_, $_[0]) }
+
+is(ref(lazy { SomeClass->new }), 'SomeClass', 'ref() returns true for refs');
+ok(!ref(lazy { "foo" }), 'ref() returns false for simple values');
+is(ref(force lazy { SomeClass->new }), 'SomeClass', 'ref() returns true for forced values');
+is(lazy { SomeClass->new }->meth, 'meth', 'method call works on deferred objects');
+is(lazy { SomeClass->new }->can('meth'), SomeClass->can('meth'), '->can works too');
+ok(lazy { SomeClass->new }->isa('SomeClass'), '->isa works too');
+is(lazy { SomeClass->new }->VERSION, SomeClass->VERSION, '->VERSION works too');
